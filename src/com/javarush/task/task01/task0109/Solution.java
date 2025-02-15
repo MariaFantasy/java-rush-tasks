2701f9c791df2703acbc24840c7eb1ad568087cb
MariaFantasy <peggy-sju@ya.ru>
2025-02-16 02:05:29 +0300

    Adding tsks solutions from module 1

---
+++ src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+Объявление и инициализация переменных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 3;
+        int b = 5;
+    }
+}
